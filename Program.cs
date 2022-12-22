424b41f51afbba92b899ee58a1657f681d1ff3be
Ammarr777 <112888184+Ammarr777@users.noreply.github.com>
2022-12-22 15:03:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Starting number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the End number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The perfect numbers: ");
+            for (int i = num1; i <= num2; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum = sum + j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.Write("\t" + i + "\n");
+                }
+            }
+            Console.WriteLine("--------------");
+        }
+    }
+}
